ebb4c546ec0e797ba5753a63c09464148881514c
Chris Hunt <c@chrishunt.co>
2015-01-01 14:00:00 -0800

    Write some horrible js to show status of orders

--- js/indiestatus.js
+++ js/indiestatus.js
@@ -1,6 +1,42 @@
-$(".spinner").addClass("hidden");
-$(".status").removeClass("hidden");
+var params = location.hash.slice(1).split("/"),
+    url = "http://localhost:5000",
+    email = params[0],
+    ids = params[1].split(",");
 
-$.post("https://209.235.214.90/setistatus/checkstatus.asp", function(data) {
-  console.log(data);
+function showError() {
+  $(".alert").html(
+    "Sorry. There was an error. Are your order numbers correct?"
+  ).removeClass("hidden");
+};
+
+function showStatus(data) {
+  $.each(data.orders, function(_, order) {
+    $(".js-status-rows").append(
+      [ '<div class="panel panel-default">',
+          '<div class="panel-heading">',
+            '<h3 class="panel-title">',
+              '<code>',
+                order.number,
+              '</code>',
+              order.progress + " " + order.message,
+            '</h3>',
+          '</div>',
+          '<div class="panel-body">',
+            order.description,
+          '</div>',
+        '</div>'
+      ].join("")
+    );
+  });
+};
+
+$.get(
+  [url, email, ids.join(",")].join("/")
+).done(function(data) {
+  $(".status").removeClass("hidden");
+  showStatus(data);
+}).fail(function() {
+  showError();
+}).always(function() {
+  $(".spinner").addClass("hidden");
 });
